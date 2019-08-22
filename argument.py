2aa3bd5bf91b6a35376c0bf55731d1503cfed0ff
Zomatree <39768508+zomatree@users.noreply.github.com>
2019-08-22 03:26:54 +0100

    Delete argument.py

--- argument.py
+++
@@ -1,8 +0,0 @@
-
-class Argument:
-    def __init__(self, name, hint):
-        self.name = name
-        self.hint = hint
-
-    def __repr__(self):
-        return f"<Argument name='{self.name}' typehint='{self.hint}'>"
